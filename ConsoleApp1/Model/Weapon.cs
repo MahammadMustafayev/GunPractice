c4b643ed2a96604507723fc5d492ecf3dfc4e1e6
Mahammad Mustafayev <tu7ekdm78@aztu.local>
2022-03-14 18:48:13 +0400

    added

---
+++ ConsoleApp1/Model/Weapon.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Model
+{
+    class Weapon
+    {
+        int bulletCapacity;
+        int currentBullets;
+        double dischargeSeconds;
+        string firingModeSingle;
+        string firingModeAuto;
+
+
+        public Weapon(int bulletCapacity)
+        {
+            this.bulletCapacity = bulletCapacity;
+        }
+
+        public Weapon(int bulletCapacity,int currentBullets):this(bulletCapacity)
+        {
+            this.currentBullets = currentBullets;
+        }
+
+        public Weapon(int bulletCapacity, int currentBullets,double dischargeSeconds):this(bulletCapacity,currentBullets)
+        {
+            this.dischargeSeconds = dischargeSeconds;
+        }
+
+        public Weapon(int bulletCapacity, int currentBullets, double dischargeSeconds,string firingModeSingle):this(bulletCapacity, currentBullets,dischargeSeconds)
+        {
+            this.firingModeSingle = firingModeSingle;
+        }
+
+        public Weapon(int bulletCapacity, int currentBullets, double dischargeSeconds, string firingModeSingle,string firingModeAuto):this(bulletCapacity, currentBullets, dischargeSeconds,firingModeSingle)
+        {
+            this.firingModeAuto = firingModeAuto;
+        }
+
+        public void Shoot()
+        {
+
+            Console.WriteLine($"1 gulle atildi");
+            currentBullets = bulletCapacity - 1;
+            Console.WriteLine($"Daraqdaki gulle sayi:{currentBullets}");
+        }
+
+        public  void Fire()
+        {
+            for (double i = 0; i < dischargeSeconds; i++)
+            {
+
+            }
+        }
+
+        public int GetRemainBulletcount()
+        {
+            return 0;
+        }
+
+        public int Reload()
+        {
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ConsoleApp1.Model;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daraqin gulle tutumunu daxil edin:");
+            Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Daraqdaki gulle sayini daxil edin:");
+            Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Daraqin bosalma saniyesini daxil edin:");
+            Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Atis modunu secin:");
+            Console.ReadLine();
+
+
+            Weapon bullet = new Weapon(30);
+            bullet.Shoot();
+        }
+    }
+}
