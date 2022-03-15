5fd6c6d69f68e9bc4fa2b15ba3b6e28c1d27d5d7
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-03-15 05:45:10 +0300

    Added

--- ConsoleApp1/Model/Weapon.cs
+++ ConsoleApp1/Model/Weapon.cs
@@ -12,6 +12,10 @@ namespace ConsoleApp1.Model
         string firingModeSingle;
         string firingModeAuto;
 
+        public Weapon()
+        {
+
+        }
 
         public Weapon(int bulletCapacity)
         {
@@ -38,30 +42,106 @@ namespace ConsoleApp1.Model
             this.firingModeAuto = firingModeAuto;
         }
 
-        public void Shoot()
+        public void GetInfo()
+        {
+            Console.WriteLine($"Darağın güllə tutumu:{bulletCapacity}");
+            Console.WriteLine($"Daraqdakı güllə sayı:{currentBullets}");
+            Console.WriteLine($"Darağın boşalma saniyəsi:{dischargeSeconds}");
+            Console.WriteLine($"Atış modları: {firingModeSingle} {firingModeAuto}");
+        }
+
+
+        public void Shoot(int currentBullets)
         {
 
             Console.WriteLine($"1 gulle atildi");
-            currentBullets = bulletCapacity - 1;
+            currentBullets = currentBullets - 1;
             Console.WriteLine($"Daraqdaki gulle sayi:{currentBullets}");
         }
 
-        public  void Fire()
+        public  void Fire(double dischargeSeconds,double dischargeSeconds1)
         {
-            for (double i = 0; i < dischargeSeconds; i++)
+            if (dischargeSeconds>0 || dischargeSeconds1>0)
             {
+                if (dischargeSeconds>=dischargeSeconds1)
+                {
+                    currentBullets = currentBullets - currentBullets;
+                    Console.WriteLine($"Silahdaki bütün güllələr {dischargeSeconds} saniyədə boşaldı.Qalan gulle sayi:{currentBullets}");
+                }
+                else if (dischargeSeconds<=dischargeSeconds1)
+                {
+                    currentBullets = currentBullets - currentBullets;
+                    Console.WriteLine($"Atəş zamanı nasazlığa görə güllələr {dischargeSeconds1} saniyədə boşaldı.Qalan gulle sayi:{currentBullets}");
 
+                }
+                else
+                {
+                    Console.WriteLine("Silahda güllə yoxdur");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Verilən zaman 0-dan kiçik ola bilməz");
             }
         }
 
         public int GetRemainBulletcount()
         {
-            return 0;
+            int count = 0;
+            for (currentBullets =0; currentBullets < bulletCapacity; currentBullets++)
+            {
+                if (currentBullets<bulletCapacity)
+                {
+                    Console.WriteLine("Ehtiyac olan gulle sayi:");
+                    count++;
+                    
+                }
+                else if (currentBullets==bulletCapacity)
+                {
+                    Console.WriteLine("Gulleye ehtiyac yoxdur");
+                    count++;
+                   
+                }
+                else
+                {
+                    Console.WriteLine("gullenin sayi daraqdaki gulle sayindan cox ola bilmez");
+                }
+                
+            }
+            return count;
         }
 
-        public int Reload()
+        public int Reload(int currentBullets,int bulletCapacity)
         {
-            return 0;
+            //int countBullets = 0;
+            //countBullets = bulletCapacity - currentBullets;
+            //countBullets = countBullets + currentBullets;
+            //Console.WriteLine($"Daraqdaki Gulle Sayi:{countBullets}");
+            while (currentBullets == 0)
+            {
+                if (currentBullets < bulletCapacity)
+                {
+                    Console.WriteLine($"Daraqda yeniden {bulletCapacity} gulle var ");
+                }
+                else
+                {
+                    Console.WriteLine($"Daraqdaki güllərin sayı {bulletCapacity} darağın tutumu qədərdir ");
+                }
+            }
+            return currentBullets;
         }
+
+        public void ChangeFireMode()
+        {
+            string firingModeSingle = "Tekli";
+            string firingModeAuto = "Avtomatik";
+            Console.WriteLine($"Atəş modu:{firingModeSingle}");
+            string firingMode = firingModeSingle.Replace(firingModeSingle,firingModeAuto);
+            Console.WriteLine("Atəş modu dəyisdi");
+            Console.WriteLine($"Atəş modu:{firingMode}");
+
+        }
+
+        
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,18 +8,70 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Daraqin gulle tutumunu daxil edin:");
-            Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Daraqdaki gulle sayini daxil edin:");
-            Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Daraqin bosalma saniyesini daxil edin:");
-            Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Atis modunu secin:");
-            Console.ReadLine();
+            Console.WriteLine("Darağın güllə tutumunu daxil edin:");
+            int bulletCapacity=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Darağdaki güllə sayini daxil edin:");
+            int currentBullets=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Darağın boşalma saniyəsini daxil edin:");
+            double dischargeSeconds=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Atəş modunu seçin:");
+            string firingModeSingle = Console.ReadLine();
+            Console.WriteLine("Atəş modunu seçin:");
+            string firingModeAuto = Console.ReadLine();
+            
 
 
-            Weapon bullet = new Weapon(30);
-            bullet.Shoot();
+            Weapon bullet = new Weapon();
+
+
+
+
+            int key = 0;
+            do
+            {
+                Console.WriteLine("Reqem daxil edin : ");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("1ci metod");
+                        bullet.Shoot(currentBullets);
+                        Console.WriteLine("----------------------------------");
+                        break;
+                    case 2:
+                        Console.WriteLine("2ci metod");
+                        bullet.Fire(dischargeSeconds, 15);
+                        Console.WriteLine("----------------------------------");
+                        break;
+                    case 3:
+                        Console.WriteLine("3cu metod");
+                        Console.WriteLine(bullet.GetRemainBulletcount());
+                        Console.WriteLine("----------------------------------");
+                        break;
+                    case 4:
+                        Console.WriteLine("4cu metod");
+                        Console.WriteLine(bullet.Reload(currentBullets, bulletCapacity)); //bullet.Reload(currentBullets,bulletCapacity);
+                        Console.WriteLine("----------------------------------");
+                        break;
+                    case 5:
+                        Console.WriteLine("5ci  metod");
+                        bullet.ChangeFireMode();
+                        Console.WriteLine("----------------------------------");
+                        break;
+                    case 6:
+                        Console.WriteLine("Proqramdan çıxdınız.Sağolun");
+                        break;
+                    case 0:
+                        Console.WriteLine("0ci  metod");
+                        bullet.GetInfo();
+                        Console.WriteLine("----------------------------------");
+                        break;
+                    default:
+                        Console.WriteLine("Səhv Daxil Etdiniz");
+                        break;
+                }
+            } while (key != 6);
+
         }
     }
 }
